7d7e4439a8acb2952edc29fbe5cd63154e1696a4
Mustafa1919 <30556958+Mustafa1919@users.noreply.github.com>
2018-02-15 22:24:47 +0300

    Add files via upload

---
+++ SıkıysaYakala/SıkıysaYakala/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SıkıysaYakala
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Random rnd1 = new Random();
+            Random rnd2 = new Random();
+
+            int en = rnd1.Next(0, this.Width - button1.Left);
+            int boy = rnd2.Next(0, this.Height - button1.Top);
+
+            button1.Left = en;
+            button1.Top = boy;
+        }
+    }
+}
