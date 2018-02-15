b822ee34f21fa72297ac659e806ce47561f00fb3
Mustafa1919 <30556958+Mustafa1919@users.noreply.github.com>
2018-02-15 22:15:12 +0300

    Add files via upload

---
+++ Kronometre/Kronometre/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kronometre
+{
+    public partial class Form1 : Form
+    {
+        int x;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = 0;
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(label1.Text);
+            label1.Text = "0";
+            timer1.Stop();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x = x + 1;
+            label1.Text = x.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+       
+
+       
+    }
+}
